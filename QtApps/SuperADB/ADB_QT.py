5b337a27d2625d24b901b33d5f8e2387b4764115
storm shadow <storm_shadow@techbliss.org>
2015-01-18 00:06:52 +0100

    fix small error

--- QtApps/SuperADB/ADB_QT.py
+++ QtApps/SuperADB/ADB_QT.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'Super_ADB2.ui'
-#
-# Created: Sat Jan 17 22:05:27 2015
-#      by: PyQt4 UI code generator 4.11.3
-#
-# WARNING! All changes made in this file will be lost!
 
 import sys
 import os
